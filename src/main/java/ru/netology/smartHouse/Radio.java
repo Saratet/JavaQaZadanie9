5cef64c1007005052a059a91a7174d7f36c0d594
Alisa <bilgeir@gmail.com>
2024-03-21 22:31:15 +0300

    Add and test radio

---
+++ src/main/java/ru/netology/smartHouse/Radio.java
@@ -0,0 +1,61 @@
+package ru.netology.smartHouse;
+
+public class Radio {
+    int currentRadioStation;
+    int currentVolume;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+        if (newCurrentRadioStation < 0) {
+            return;
+        }
+        if (newCurrentRadioStation > 9) {
+            return;
+        }
+        currentRadioStation = newCurrentRadioStation;
+    }
+
+    public void next() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+            return;
+        }
+        currentRadioStation = 0;
+    }
+    public void prev() {
+        if (currentRadioStation >= 1) {
+            currentRadioStation = currentRadioStation - 1;
+            return;
+        }
+        currentRadioStation = 9;
+    }
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+    public void increaseVolume() {
+        if (currentVolume > 99) {
+            return;
+        }
+        currentVolume = currentVolume + 1;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume < 1) {
+            return;
+        }
+        currentVolume = currentVolume - 1;
+    }
+}
---
+++ src/test/java/ru/netology/smartHouse/RadioTest.java
@@ -0,0 +1,172 @@
+package ru.netology.smartHouse;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shouldSetRadioStationWithinRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(5);
+
+        int expected = 5;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetRadioStationBelowRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetRadioStationAboveRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNextRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(5);
+        radio.next();
+        int expected = 6;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNextRadioStationFrom9() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(9);
+        radio.next();
+        int expected = 0;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetPreviousRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(7);
+        radio.prev();
+        int expected = 6;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetPreviousRadioStationFrom0() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(0);
+        radio.prev();
+        int expected = 9;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetPreviousRadioStationFrom9() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(9);
+        radio.prev();
+        int expected = 8;
+        int actual = radio.getCurrentRadioStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetVolumeWithinRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(27);
+
+        int expected = 27;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeAboveRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(108);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeBelowRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolumeInRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(32);
+        radio.increaseVolume();
+        int expected = 33;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotIncreaseVolumeOutOfRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseVolumeInRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(45);
+        radio.decreaseVolume();
+        int expected = 44;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotDecreaseVolumeOutOfRange() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+}
